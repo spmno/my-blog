2ab481ecd37ebb97aae17aeb69f229421b4780ee
spmno <sunqingpeng@hotmail.com>
2024-12-17 14:03:11 +0800

    add vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,14 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.xiachedan.cn',
-  integrations: [svelte(), tailwind(), sitemap()]
+  integrations: [svelte(), tailwind(), sitemap()],
+  output: 'static',
+  adapter: vercel({
+    imageService: true,
+  }),
 });
